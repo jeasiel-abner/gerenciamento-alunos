2c9a6f65d194f0fe298f4cc828af537c381bd711
jeasiel <jeasielabner@gmail.com>
2025-07-01 10:53:36 -0300

    Iniciei a parte dos testes de integração mas não consegui entender bem como funcionam os Controllers (no meu projeto de POO não retornavam nada, era mais ou menos uma bagunça), logo não consegui implementar, apenas iniciei a implementação.

--- src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
@@ -1,10 +1,36 @@
 package br.com.gerenciamento.controller;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AlunoControllerTest {
+    // quatro testes de integração
+
+    @Autowired
+    private AlunoController alunoController;
+
+    @Test
+    public void a(){
+
+    }
+
+    @Test
+    public void b(){
+        
+    }
+
+    @Test
+    public void c(){
+        
+    }
+
+    @Test
+    public void d(){
+        
+    }
 }
